b643351641d3f2e3b830a5e31ee96a35f5b68636
InnaIvBoiko <inna_boiko@libero.it>
2024-07-28 18:46:34 +0200

    components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,15 @@
+import payments from '../../payments.json';
 //import css from './App.module.css';
+
+import PaymentList from '../PaymentLis/PaymentLis.jsx';
+import PageTittle from '../PageTitle/PageTitle.jsx';
 
 export default function App() {
 
   return (
     <>
-      <h1>Hello world!!!</h1>
+      <PageTittle text="Payments" />
+      <PaymentList payments={payments} />
     </>
-  )
+  );
 }
